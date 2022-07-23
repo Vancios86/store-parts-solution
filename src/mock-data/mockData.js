aa440dde7906c8e4ac608e203a3f5d4c09a0fe86
Ion Albu <ialbu.webdev@gmail.com>
2022-07-23 20:00:47 +0100

    style improvements + miscellanous small changes

--- src/mock-data/mockData.js
+++ src/mock-data/mockData.js
@@ -1,53 +1,53 @@
-export const storeItems = [
+export const mockItems = [
   {
     name: 'Mouse 1',
     price: '112.00$',
-    type: 'Mouse',
+    type: 'mouse',
   },
   {
     name: 'Mouse 2',
     price: '35.99$',
-    type: 'Mouse',
+    type: 'mouse',
   },
   {
     name: 'Keyboard 1',
     price: '15.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
   {
     name: 'Keyboard 2',
     price: '154.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
   {
     name: 'Keyboard 3',
     price: '83.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
   {
     name: 'Monitor 1',
     price: '531.00$',
-    type: 'Monitor',
+    type: 'monitor',
   },
   {
     name: 'Monitor 2',
     price: '531.00$',
-    type: 'Monitor',
+    type: 'monitor',
   },
   {
     name: 'Monitor 3',
     price: '222.00$',
-    type: 'Monitor',
+    type: 'monitor',
   },
   {
     name: 'Monitor 4',
     price: '123.00$',
-    type: 'Monitor',
+    type: 'monitor',
   },
   {
     name: 'Mousepad 1',
     price: '10.00$',
-    type: 'Mousepad',
+    type: 'mousepad',
   },
   {
     name: 'Mousepad 2',
@@ -60,16 +60,16 @@ export const keyboards = [
   {
     name: 'Keyboard 1',
     price: '15.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
   {
     name: 'Keyboard 2',
     price: '154.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
   {
     name: 'Keyboard 3',
     price: '83.00$',
-    type: 'Keyboard',
+    type: 'keyboard',
   },
 ];
